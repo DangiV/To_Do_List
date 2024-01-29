1d3ef3ba580b55ae93a65ef26e84c1da533e098a
vikas <vikasdangi273@gmail.com>
2024-01-29 16:18:56 +0530

    edit todo list

--- src/Component/ToDoList.jsx
+++ src/Component/ToDoList.jsx
@@ -1,40 +1,67 @@
 import React, { useState } from 'react'
 
 const ToDoList = () => {
-    const [enterData, setEnterData] = useState('')
-    const [dataAdded, setDataAdded] = useState([])
+    const [inputData, setInputData] = useState("")
+    const [items, setItems] = useState([])
+    const [toggle, setToggle] = useState(true)
+    const [editId, setEditId] = useState(null)
 
-    const DataEntered = (e) => {
-        setEnterData(e.target.value)
+    const AddItem = () => {
+        if (!inputData) {
+        }
+        else if (inputData && !toggle) {
+            setItems(
+                items.map((item) => {
+                    if (item.id === editId) {
+                        return { ...item, name: inputData }
+                    }
+                    return item;
+                })
+            )
+            setToggle(true)
+            setInputData("")
+            setEditId(null)
+        }
+        else {
+            const newItem = { id: new Date().getTime(), name: inputData }
+            setItems([...items, newItem])
+            setInputData("")
+        }
     }
 
-    const AddedData = () => {
-        setDataAdded((oldData) => {
-            return [...oldData, enterData]
+    const EditItems = (id) => {
+        const AfterEdit = items.find((item) => {
+            return item.id === id
         })
-        setEnterData("")
+        setToggle(false)
+        setInputData(AfterEdit.name)
+        setEditId(id)
+    }
+    const RemoveItem = (id) => {
+        const newItems = items.filter((item) => {
+            return id !== item.id
+        })
+        setItems(newItems)
     }
 
-    const RemoveData = (id) => {
-        setDataAdded((oldItem) => {
-            return oldItem.filter((item, index) => {
-                return index !== id
-            })
-        })
-    }
     return (
         <>
             <h1>To Do List</h1>
-            <input placeholder='enter item' value={enterData} onChange={DataEntered} />
-            <button onClick={() => AddedData()}>Add</button>
+
+            <input type="text" placeholder='enter item' value={inputData} onChange={(e) => setInputData(e.target.value)} />
+            {toggle ? <button onClick={() => AddItem()}>Add</button> : <button onClick={() => AddItem()}>Update</button>}
 
             <ol>
                 <ul>
-                    {dataAdded.map((item, index) => {
-                        return <li key={index}>{item} <button onClick={() => RemoveData(index)}>Remove</button></li>
+                    {items.map((item) => {
+                        return <li key={item.id}>{item.name}
+                            <button onClick={() => RemoveItem(item.id)}>remove</button>
+                            <button onClick={() => EditItems(item.id)}>edit</button>
+                        </li>
                     })}
                 </ul>
             </ol>
+
         </>
     )
 }
